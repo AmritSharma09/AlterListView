ea1f1108b220a4a22fc42605e940b856db4eae5b
AmritSharma09 <amrit.121040@gmail.com>
2017-05-17 01:10:28 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -13,11 +13,16 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+ //Declare all Variable of Visible component
+   
     EditText empName;
     Button add;
     ListView lv;
+    
+  //Declare ArrayList which hold String data
     ArrayList<String> al;
+    
+ // Declare A Adapter from where we get data in ListView   
     ArrayAdapter<String> a;
 
 
@@ -25,50 +30,55 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//Initialization of all visible variable 
         empName=(EditText)findViewById(R.id.empName);
         add=(Button) findViewById(R.id.add);
         lv=(ListView)findViewById(R.id.list);
 
+//Initialization Of ArrayList        
         al=new ArrayList<String>();
+        
+//Initialization of ArrayAdapter
         a=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,al);
-        lv.setAdapter(a);
+        
+//Set data in listView from Arrayadapter
+       lv.setAdapter(a);
 
+   //When we long Click on any ListView Item that item will be Deleted if it found text in listView Item     
         lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
-               if(al.get(i)==""){
-                   a.notifyDataSetChanged();
-                   Toast.makeText(getApplicationContext(),"Invalid Cell",Toast.LENGTH_SHORT).show();
+               if(al.get(i)==""){   //Check if ListItem text in nothing
+                   a.notifyDataSetChanged();  //notify ArrayAdapter
+                   Toast.makeText(getApplicationContext(),"Invalid Cell",Toast.LENGTH_SHORT).show(); //print in toast that it is invalid cell
 
                }else{
-                   al.remove(i);
-                   al.remove(i++);
-                   a.notifyDataSetChanged();
+                   //If in listView Item it get any Text
+                   al.remove(i); //remove text at that position 
+                   al.remove(i++); // remove respect blank created ListVie Item also
+                   a.notifyDataSetChanged(); // Notify ArratyAdapter after All these Operation
                    Toast.makeText(getApplicationContext(),"Valid Cell Deleted with its respective cell",Toast.LENGTH_SHORT).show();
                }
 
-                  /* al.remove(i);
-                a.notifyDataSetChanged();
-                Toast.makeText(getApplicationContext(),"Selected row Deleted",Toast.LENGTH_SHORT).show();*/
-
-                // Return fro here
+                
                 return true;
             }
         });
 
 
+ // Giveing tast when we click on Add Button       
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // Take data from EditText where we write any name and set in string variable name 
                 String name=empName.getText().toString();
-                al.add(name);
-                al.add("");
+                al.add(name); // Store in arrayList
+                al.add("");//Also add a blank data 
 
-                a.notifyDataSetChanged();
-                empName.setText("");
+                a.notifyDataSetChanged(); // notify adapter 
+                empName.setText(""); // after all these clear EditText
 
-                empName.requestFocus();
+                empName.requestFocus(); // Again Set Cursor on EditText to give name
             }
         });
 
